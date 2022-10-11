2faa131240ad023c04e126541f735148c7838d61
BaptisteMGit <baptiste.menetrier@ensta-bretagne.org>
2022-10-11 15:58:37 +0200

    Hourly folder creation, split NMEA and RAWX
    Script adapté du script du projet 2021-2022. Les messages NMEA et RAWx sont enregistrés de manière indépendantes. Les messages RAWx sont sauvegardés en continu, les trames NMEA sont lues toutes les 10 minutes.

--- acquisition.py
+++ acquisition.py
@@ -2,24 +2,54 @@ import os
 import time
 import shutil
 from datetime import datetime
-time.sleep(120)
+# time.sleep(120) # ? 
 #print(os.getcwd())
 
-c = 0
-path = './'
-#folder_exists = os.path.exists(path + 'acquisition')
+# Path definition
+root = r'C:\Users\33686\Desktop\ENSTAB\Cours\3A\Projet Guerledan\testPy'
+pathAcquisition = f'{root}/acquisition'
+# pathAcquisition = f'{root}\\acquisition' # Windows
 
-os.system('rm -r ./acquisition')
 
-os.system('mkdir ./acquisition')
-    
-while True:
-    now = datetime.now()
-    current_time = now.strftime("%d_%m_%Y_%H:%M:%S")
+T_NMEA = 10 # Save NMEA every 10 min
 
-    name = str(c)
-    os.system(('gpspipe -o ~/acquisition/{}_{}.txt -r -R -n 15000').format(name, current_time))
-    c+=1
+# Remove former folder 'acquisition'
+os.system(f'rm -r {pathAcquisition}')
+
+# Create new folder 'acquisition'
+os.system(f'mkdir {pathAcquisition}')
+
+
+# while True:
+now = datetime.now()
+current_min = now.minute
+current_sec = now.second
+
+pathfolderHour = f'{pathAcquisition}/{now.strftime("%d%m%Y_%H00")}'
+# pathfolderHour = f'{pathAcquisition}\{now.strftime("%d%m%Y_%H00")}'
+
+if ~os.path.exists(pathfolderHour):
+    os.system(f'mkdir {pathfolderHour}')
+
+filename = now.strftime("%d%m%Y_%H00")
+outputFilePath_RAW = f'{pathfolderHour}/{filename}_RAW.txt'
+outputFilePath_NMEA = f'{pathfolderHour}/{filename}_NMEA.txt'
+
+# outputFilePath_RAW = f'{pathfolderHour}\{filename}_RAW.txt'
+# outputFilePath_NMEA = f'{pathfolderHour}\{filename}_NMEA.txt'
+
+cmdRAW =  f'gpspipe -o {outputFilePath_RAW} -R'
+cmdNMEA =  f'gpspipe -o {outputFilePath_NMEA} -r'
+
+# Save RAW
+# os.system(cmdRAW)
+print(cmdRAW)
+
+# Save NMEA 
+if (current_min%T_NMEA == 0) & (current_sec == 0):
+    # os.system(cmdNMEA)
+    print(cmdNMEA)
+
     
 
  
