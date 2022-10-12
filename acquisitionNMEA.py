9735300cce9c11be4e9c4221bb6e9bf9bbb64383
BaptisteMGit <baptiste.menetrier@ensta-bretagne.org>
2022-10-12 14:51:35 +0200

    Création de 2 scripts d'acquisitions distincts NMEA RINEX

---
+++ acquisitionNMEA.py
@@ -0,0 +1,52 @@
+import os
+import time
+import shutil
+from datetime import datetime
+
+# time.sleep(120) # Wait for synchronisation 
+
+# Path definition
+root = r'.'
+pathData = f'{root}/data/NMEA'
+
+T_NMEA = 10 # Save NMEA every 10 min
+
+# Remove former folder 'acquisition'
+os.system(f'rm -r {pathData}')
+
+# Create new folder 'acquisition'
+os.system(f'mkdir {pathData}')
+
+createFolderHour = True 
+pathFolderHour = ''
+
+def main():
+
+    while True:
+        previousPathFolderHour = pathFolderHour
+        now = datetime.now()
+        current_min = now.minute
+        current_sec = now.second
+
+        pathFolderHour = f'{pathData}/{now.strftime("%d%m%Y_%H00")}'
+        if pathFolderHour != previousPathFolderHour:
+            createFolderHour = True
+
+        if createFolderHour:
+            os.system(f'mkdir {pathFolderHour}')
+            createFolderHour = False
+
+        filename = now.strftime("%d%m%Y_%H00")
+        outputFilePath_NMEA = f'{pathFolderHour}/{filename}_NMEA.txt'
+
+        cmdNMEA =  f'gpspipe -o {outputFilePath_NMEA} -r'
+
+        # Save NMEA 
+        if (current_min%T_NMEA == 0) & (current_sec == 0):
+            os.system(cmdNMEA)
+
+
+if __name__ == '__main__':
+    main()    
+
+ 
---
+++ acquisitionRINEX.py
@@ -0,0 +1,48 @@
+import os
+import time
+import shutil
+from datetime import datetime
+
+# time.sleep(120) # Wait for synchronisation 
+
+# Path definition
+root = r'.'
+
+pathData = f'{root}/data/RINEX'
+
+# Remove former folder 'acquisition'
+os.system(f'rm -r {pathData}')
+# Create new folder 'acquisition'
+os.system(f'mkdir {pathData}')
+
+
+createFolderHour = True 
+pathFolderHour = ''
+
+def main():
+    while True:
+        previousPathFolderHour = pathFolderHour
+        now = datetime.now()
+
+        pathfolderHour = f'{pathData}/{now.strftime("%d%m%Y_%H00")}'
+        if pathFolderHour != previousPathFolderHour:
+            createFolderHour = True
+
+        if createFolderHour:
+            os.system(f'mkdir {pathFolderHour}')
+            createFolderHour = False
+
+        filename = now.strftime("%d%m%Y_%H00")
+        outputFilePath_RINEX = f'{pathfolderHour}/{filename}_RINEX.txt'
+
+        cmdRINEX =  f'gpsrinex -f {outputFilePath_RINEX}'
+
+        # Save RINEX 
+        os.system(cmdRINEX)
+
+    
+if __name__ == "__main__":
+    main()
+
+
+ 
