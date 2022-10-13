19db445f5303df88a5a751c68d56e88e62deeacc
BaptisteMGit <baptiste.menetrier@ensta-bretagne.org>
2022-10-13 11:09:40 +0200

    Update to write logs

--- capteur.py
+++ capteur.py
@@ -1,5 +1,6 @@
 import bme280
 import smbus2
+import datetime
 from time import sleep
 
 port = 1
@@ -8,10 +9,30 @@ bus = smbus2.SMBus(port)
 
 bme280.load_calibration_params(bus, address)
 
+Ts = 10 # Sampling period (should be the same as RINEX one)
+
+file = './data/METEO/logMETEO.txt'
+# Write header 
+with open(file, 'w') as f:
+    header = "date, temperature_C, pression_hPa, humidity_per\n"
+    f.write(header)
+
 while True:
+    # Get sensor data
     bme280_data = bme280.sample(bus, address)
     humidity  = bme280_data.humidity
     pressure  = bme280_data.pressure
-    ambient_temperature = bme280_data.temperature
-    print(f'H:{humidity}, P:{pressure}, T:{ambient_temperature}')
-    sleep(1)
+    temperature = bme280_data.temperature
+
+    # Get current date
+    now = datetime.now()
+    date = now.strftime("%d%m%Y %H:%M:%S")
+
+    # Write log
+    with open(file, 'w') as f:
+        line = f"{date}, {temperature}, {pressure}, {humidity}\n"
+        f.write(line)
+
+    sleep(Ts)
+
+
