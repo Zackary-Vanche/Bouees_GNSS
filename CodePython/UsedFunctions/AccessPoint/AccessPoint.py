58c4ed860bc32247deae2828fe8fd8b96212d757
BaptisteMGit <baptiste.menetrier@ensta-bretagne.org>
2022-11-07 18:43:14 +0100

    Réorganisation des fichiers et ajout des fichiers pour la gestion du point d'accès RPiZero W

---
+++ CodePython/UsedFunctions/AccessPoint/AccessPoint.py
@@ -0,0 +1,72 @@
+import time
+import os
+
+
+SSID = 'RPiZeroW_Bouee'
+INTERFACE = 'Wi-Fi'
+PROFILE = r'C:\Users\33686\Desktop\ENSTAB\Cours\3A\Guerledan\Bouees_GNSS\CodePython\UsedFunctions\AccessPoint\Wi-Fi-RPiZeroW_Bouee.xml'
+
+def connnectToAP():
+    """ Try to connect to buyo access point if available. """
+    buyoConnected = False
+
+    cmd0 = f'netsh wlan add profile filename="{PROFILE}"'
+    status = os.system(cmd0)
+
+    while not buyoConnected:
+        cmd1 = f'netsh wlan show networks | findstr SSID | findstr {SSID}'
+        status = os.system(cmd1)
+        # print(status)
+
+        if not status:
+            # Buyo AP is available 
+            print("Attempting to connect to buyo..\n")
+            cmd2 = f'netsh wlan connect name="{SSID}" interface="{INTERFACE}"'
+            try:
+                status = os.system(cmd2)
+                buyoConnected = True
+                print("Connection to buyo successful !\n")
+            except:
+                print("Connection to buyo failed !\n")
+        else:
+            # Buyo AP is out of range 
+            print("Buyo out of range.\n")
+
+        time.sleep(5)
+
+def disconnectFromAP():
+    """ Disconnect from buyo access point if connected. """
+
+    cmd1 = f'netsh wlan show interfaces | findstr SSID | findstr {SSID}'
+    status = os.system(cmd1)
+    # print(status)
+
+    if not status:
+        # User is connected to Buyo 
+        print("Disconnecting from buyo..\n")
+        cmd2 = 'netsh wlan disconnect'
+        try:
+            status = os.system(cmd2)
+            print("Disconnected from buyo.\n")
+        except:
+            print("Disconnection from buyo failed !\n")
+    else:
+        # User is not connected to buyo 
+        print("Buyo not connected.\n")
+
+        time.sleep(5)
+
+def forgetAP():
+    """ Forget Access Point settings"""
+
+    cmd = f'netsh wlan delete profile name={SSID}'
+    status = os.system(cmd)
+
+if __name__=="__main__":
+
+    forgetAP()
+    connnectToAP()
+    disconnectFromAP()
+
+
+
