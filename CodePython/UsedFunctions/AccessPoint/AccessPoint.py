7bf96f1b6212d939ffaed99c08b2bcd4458ec748
BaptisteMGit <baptiste.menetrier@ensta-bretagne.org>
2022-11-09 22:42:01 +0100

    Add two functions to transfert files from AP to local

--- CodePython/UsedFunctions/AccessPoint/AccessPoint.py
+++ CodePython/UsedFunctions/AccessPoint/AccessPoint.py
@@ -5,6 +5,7 @@ import os
 SSID = 'RPiZeroW_Bouee'
 INTERFACE = 'Wi-Fi'
 PROFILE = r'C:\Users\33686\Desktop\ENSTAB\Cours\3A\Guerledan\Bouees_GNSS\CodePython\UsedFunctions\AccessPoint\Wi-Fi-RPiZeroW_Bouee.xml'
+PASSWORD = "hydro"
 
 def connnectToAP():
     """ Try to connect to buyo access point if available. """
@@ -62,11 +63,39 @@ def forgetAP():
     cmd = f'netsh wlan delete profile name={SSID}'
     status = os.system(cmd)
 
+def transferFileFromAP(src, dest):
+    "Transfer file from AP to local"
+    # cmd = f"pscp -batch -pw {PASSWORD} pi@raspberrypi.local:{src} {dest}"
+    cmd = f"pscp -pw {PASSWORD} pi@raspberrypi.local:{src} {dest}"
+
+    status = os.system(cmd) 
+
+def getDataFromAP(dataType='all'):
+    """ Download all archived data from AP"""
+    # NMEA 
+    if dataType == 'NMEA' or dataType == 'all':
+        src = "/home/pi/BuyoData/NMEA/*.gz"
+        dest = r"C:\Users\33686\Desktop\ENSTAB\Cours\3A\Guerledan\CONFIG"
+        transferFileFromAP(src, dest)
+
+    if dataType == 'UBX' or dataType == 'all':
+        src = "/home/pi/BuyoData/UBX/*.gz"
+        dest = r"C:\Users\33686\Desktop\ENSTAB\Cours\3A\Guerledan\CONFIG"
+        transferFileFromAP(src, dest)
+
+    if dataType == 'METEO' or dataType == 'all':
+        src = "/home/pi/BuyoData/METEO/*.gz"
+        dest = r"C:\Users\33686\Desktop\ENSTAB\Cours\3A\Guerledan\CONFIG"
+        transferFileFromAP(src, dest)
+
 if __name__=="__main__":
 
-    forgetAP()
-    connnectToAP()
-    disconnectFromAP()
+    # forgetAP()
+    # disconnectFromAP()
+    # connnectToAP()
 
+    # src = "/home/pi/BuyoConfig/APRoutine.sh"
+    # dest = r"C:\Users\33686\Desktop\ENSTAB\Cours\3A\Guerledan\CONFIG"
+    # transferFileFromAP(src, dest)
 
-
+    getDataFromAP(dataType='NMEA')
